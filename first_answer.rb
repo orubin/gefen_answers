e36d3fb85ae8afefcef7d0d2806d173c284f76d4
Or Rubin <or.rubin@hotmail.com>
2017-11-19 19:42:48 +0200

    First answer

---
+++ first_answer.rb
@@ -0,0 +1,17 @@
+def print_arrays(arrays)
+  puts arrays.flatten.join(' ')
+end
+
+print_arrays([[], [], []])
+
+print_arrays([[]])
+
+print_arrays([[1]])
+
+print_arrays([[1], [2, 3, 4], []])
+
+print_arrays([[1, 2, 3], [4], [], []])
+
+
+
+
